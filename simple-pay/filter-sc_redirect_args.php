08b5f352d2970acffb72f83241e9f8d6638f8f62
Nick Young <mycorpweb@gmail.com>
2015-08-14 13:39:41 -0600

    Added sc_redirect_args

--- simple-pay/filter-sc_redirect_args.php
+++ simple-pay/filter-sc_redirect_args.php
@@ -1,3 +1,12 @@
 <?php
 
-// TODO
+// Add additional arguments to pass along with the redirect
+// Uses the current args being passed (you should try to avoid modifying these) and passes the charge as well
+function sc_redirect_args_example( $args, $charge ) {
+
+    // Append the customer ID  to the redirect URL
+    $args['cust_id'] = $charge->customer;
+
+    return $args;
+}
+add_filter( 'sc_redirect_args', 'sc_redirect_args_example', 10, 2 );
