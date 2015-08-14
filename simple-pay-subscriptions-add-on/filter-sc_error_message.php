c10fa7b9e67a2020c237faeceb79be927315b68b
Nick Young <mycorpweb@gmail.com>
2015-08-14 13:52:23 -0600

    Added sc_error_message example

--- simple-pay-subscriptions-add-on/filter-sc_error_message.php
+++ simple-pay-subscriptions-add-on/filter-sc_error_message.php
@@ -1,3 +1,7 @@
 <?php
 
-// TODO
+// Change the default error message text
+function sc_error_message_example() {
+    return '<p>An error has occurred. Please contact a <a href="htttp://linktocontactpage.com">site administrator</a>.</p>';
+}
+add_filter( 'sc_error_message', 'sc_error_message_example' );
