5d30087155e4a7c921de8d4ca32b94b227f0b4fa
Nick Young <mycorpweb@gmail.com>
2015-08-14 13:17:03 -0600

    Removed pro file since it applies to lite also. Moved example to lite file

--- simple-pay/action-sc_after_charge.php
+++ simple-pay/action-sc_after_charge.php
@@ -1,3 +1,8 @@
 <?php
 
-// TODO
+// Takes the $charge_response as a parameter so you can pull information from the charge
+function sc_after_charge_example( $charge_response ) {
+    // Useful for adding additional functionality after the charge is complete
+    // For example something like storing the transaction ID in the database if you need to
+}
+add_action( 'sc_after_charge', 'sc_after_charge_example' );
