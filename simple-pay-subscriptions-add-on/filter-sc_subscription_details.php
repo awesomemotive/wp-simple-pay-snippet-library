04a13bf55eef4a345bef11bf188860018db13c7a
Nick Young <mycorpweb@gmail.com>
2015-08-14 14:05:53 -0600

    Added sc_subscription_details example

--- simple-pay-subscriptions-add-on/filter-sc_subscription_details.php
+++ simple-pay-subscriptions-add-on/filter-sc_subscription_details.php
@@ -1,3 +1,11 @@
 <?php
 
-// TODO
+// Change how the subscription details can be shown
+function sc_subscription_details_example( $details_html, $sub )  {
+
+    // Show just the plan name and not all the extra details
+    $details_html = $sub->name;
+
+    return $details_html;
+}
+add_filter( 'sc_subscription_details', 'sc_subscription_details_example', 10, 2 );
