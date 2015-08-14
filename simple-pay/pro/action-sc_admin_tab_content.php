0d384a2355aad7e59ed0afe09c1a423eb7285383
Nick Young <mycorpweb@gmail.com>
2015-08-14 12:32:23 -0600

    Updated sc_admin_tab_content action

--- simple-pay/pro/action-sc_admin_tab_content.php
+++ simple-pay/pro/action-sc_admin_tab_content.php
@@ -1,3 +1,12 @@
 <?php
 
-// TODO
+// Hook into the Admin tab to add additional content, settings, etc
+function sc_admin_tab_content_example() {
+    // Add a label and a text box
+    $html = '<label for="sc_custom_admin_box">Example Textbox</label><br>';
+    $html .= '<input type="text" id="sc_custom_admin_box" />';
+
+    echo $html;
+
+}
+add_action( 'sc_admin_tab_content', 'sc_admin_tab_content_example' );
