98870d3cbd0517e47ec15d1df98c05982fb2351a
Nick Young <mycorpweb@gmail.com>
2015-08-14 12:59:25 -0600

    Added example for sc_settings_tab_license

--- simple-pay/pro/action-sc_settings_tab_license.php
+++ simple-pay/pro/action-sc_settings_tab_license.php
@@ -1,4 +1,11 @@
 <?php
 
+// Hook into the admin licenses tab to add additional content, settings, etc
+function sc_settings_tab_license_example() {
+    // Add a label and a text box
+    $html = '<label for="sc_custom_admin_box">Example Textbox</label><br>';
+    $html .= '<input type="text" id="sc_custom_admin_box" />';
 
-// TODO
+    echo $html;
+}
+add_action( 'sc_settings_tab_license', 'sc_settings_tab_license_example' );
