cbcb327015921f0a47031ed8bd1c07e900634987
Nick Young <mycorpweb@gmail.com>
2015-09-06 14:18:39 -0600

    Added Pro CSS filters

---
+++ simple-pay/pro/CSS Class Filters/filter-sc_coup_coupon_class.php
@@ -0,0 +1,19 @@
+<?php
+
+// Add or change the classes for fields using the 'sc-coup-coupon' class
+
+// Add a class in addition to the default class.
+function sc_coup_coupon_class_example( $default ) {
+// Need to add the space in manually here
+    return $default . ' ' . 'my-css-class';
+
+// Another optional way is adding the space at the beginning of the class you want to insert
+// return $default . ' my-css-class';
+}
+add_filter( 'sc_coup_coupon_class', 'sc_coup_coupon_class_example' );
+
+// Don't use the default class and overwrite with multiple classes of your own
+function sc_coup_coupon_class_example_2() {
+    return 'my-css-class-1 my-class-2 my-class-3';
+}
+add_filter( 'sc_coup_coupon_class', 'sc_coup_coupon_class_example_2' );
---
+++ simple-pay/pro/CSS Class Filters/filter-sc_uea_currency_class.php
@@ -0,0 +1,19 @@
+<?php
+
+// Add or change the classes for fields using the 'sc-uea-currency' class
+
+// Add a class in addition to the default class.
+function sc_uea_currency_class_example( $default ) {
+// Need to add the space in manually here
+    return $default . ' ' . 'my-css-class';
+
+// Another optional way is adding the space at the beginning of the class you want to insert
+// return $default . ' my-css-class';
+}
+add_filter( 'sc_uea_currency_class', 'sc_uea_currency_class_example' );
+
+// Don't use the default class and overwrite with multiple classes of your own
+function sc_uea_currency_class_example_2() {
+    return 'my-css-class-1 my-class-2 my-class-3';
+}
+add_filter( 'sc_uea_currency_class', 'sc_uea_currency_class_example_2' );
