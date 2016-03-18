0b0981073acb5067b0ec5f02e0572f11ef087180
Phil Derksen <pderksen@gmail.com>
2016-03-17 20:53:37 -0700

    Include pre-filled email example for sc_modify_script_options filter.

--- simple-pay/pro/filter-sc_modify_script_options.php
+++ simple-pay/pro/filter-sc_modify_script_options.php
@@ -1,12 +1,24 @@
 <?php
 
-/*
- * Filter that allows changing of the script variables passed to create the checkout form
+/**
+ * Filter that allows changing of the script variables passed to create the checkout form.
+ * Example 1: Modify the item description.
  */
-function sc_modify_script_options_example( $options ) {
+function sc_modify_script_options_example_1( $options ) {
     // Change the description to include the page title
     $options['script']['description'] = 'Description - ' . get_the_title();
 
     return $options;
 }
-add_filter( 'sc_modify_script_options', 'sc_modify_script_options_example' );
+add_filter( 'sc_modify_script_options', 'sc_modify_script_options_example_1' );
+
+/**
+ * Example 2: Modify the pre-filled email.
+ */
+function sc_modify_script_options_example_2( $options ) {
+    // Change the description to include the page title
+    $options['script']['email'] = 'support@wpsimplepay.com';
+
+    return $options;
+}
+add_filter( 'sc_modify_script_options', 'sc_modify_script_options_example_2' );
