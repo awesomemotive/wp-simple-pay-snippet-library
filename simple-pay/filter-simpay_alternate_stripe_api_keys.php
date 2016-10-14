b9342b35718e46511cd24d067eabf4e23f08268d
Phil Derksen <pderksen@gmail.com>
2016-10-14 10:52:59 -0700

    Comments.

--- simple-pay/filter-simpay_alternate_stripe_api_keys.php
+++ simple-pay/filter-simpay_alternate_stripe_api_keys.php
@@ -3,6 +3,7 @@
 // Use alternate Stripe accounts (API keys) on specific pages.
 
 function simpay_alt_stripe_api_secret_key( $secret_key, $test_mode ) {
+
 	// In this example, the specified alternate Stripe API keys will be used on the PAGE with the slug "alt-payment-page".
 	// Add specific payment success and failure page slugs that will use the alternate Stripe API keys if custom redirect pages are used.
 	// Replace with conditional logic you require.
@@ -15,12 +16,15 @@ function simpay_alt_stripe_api_secret_key( $secret_key, $test_mode ) {
 		}
 	}
 
+	// Repeat if using even more Stripe accounts.
+
 	return $secret_key;
 }
 add_filter( 'simpay_secret_key', 'simpay_alt_stripe_api_secret_key', 10, 2 );
 
 function simpay_alt_stripe_api_publishable_key( $secret_key, $test_mode ) {
 
+	// Match conditional check from simpay_alt_stripe_api_secret_key().
 	if ( is_page( 'alt-payment-page' ) ) {
 		if ( true == $test_mode ) {
 			$secret_key = 'pk_test_XXX';
@@ -28,6 +32,8 @@ function simpay_alt_stripe_api_publishable_key( $secret_key, $test_mode ) {
 			$secret_key = 'pk_live_XXX';
 		}
 	}
+
+	// Repeat if using even more Stripe accounts.
 
 	return $secret_key;
 }
