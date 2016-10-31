b90284887f98f481fe8ee855d0f862a818328cf7
Phil Derksen <pderksen@gmail.com>
2016-10-31 14:47:02 -0700

    Add custom plugin to override TLS 1.2 requirement by Stripe.

---
+++ custom-plugins/wp-simple-pay-override-tls12-requirement.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name: WP Simple Pay - Override TLS 1.2 requirement.
+ * Description: [Custom Plugin for SITENAME.com] Override TLS 1.2 requirement by Stripe.
+ */
+
+// See https://github.com/stripe/stripe-php#ssl--tls-compatibility-issues
+function simpay_override_tls12_requirement() {
+	if ( class_exists( 'Stripe\Stripe' ) ) {
+		$curl = new \Stripe\HttpClient\CurlClient( array( CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1 ) );
+		\Stripe\ApiRequestor::setHttpClient( $curl );
+	}
+}
+add_action( 'init', 'simpay_override_tls12_requirement', 1, 0 );
