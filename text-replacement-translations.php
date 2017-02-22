fed2d928c25c1848074be693bc0a58bea718dc9b
Phil Derksen <pderksen@gmail.com>
2017-02-22 14:55:11 -0800

    Update to text replacement/translation example.

--- text-replacement-translations.php
+++ text-replacement-translations.php
@@ -5,18 +5,21 @@
  * Alternatively use a translation plugin such as https://wordpress.org/plugins/loco-translate/.
  * In this example, we'll change the coupon "Apply" button (and "remove" link) text.
  */
-function simpay_text_replacement( $translations, $text, $domain ) {
+function simpay_text_replacement( $translated_text, $text, $domain ) {
 
 	if ( 'stripe' == $domain ) {
-		if ( 'Apply' == $text ) {
-			return __( 'Apply coupon' );
-		}
-		if ( 'remove' == $text ) {
-			return __( 'remove coupon' );
+
+		switch ( $translated_text ) {
+			case 'Apply' :
+				$translated_text = __( 'NEW APPLY BUTTON TEXT', 'stripe' );
+				break;
+			case 'remove' :
+				$translated_text = __( 'NEW REMOVE LINK TEXT', 'stripe' );
+				break;
 		}
 	}
 
-	return $translations;
+	return $translated_text;
 }
 
 add_filter( 'gettext', 'simpay_text_replacement', 10, 3 );
