ca0651894c500ecc0e143dc199862eff5f2a2c9c
Phil Derksen <pderksen@gmail.com>
2017-03-29 19:29:18 -0700

    Placeholder for 3.0 custom locale, date format and currency settings per form snippet.

---
+++ custom-locale-date-format-currency-per-form.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: WP Simple Pay - Custom locale, date format and currency settings per form.
+ * Plugin URI: https://wpsimplepay.com
+ * Description: Custom locale, date format, currency, currency position and decimal/thousands separator.
+ * Version: 1.0
+ */
+
+/**
+ * In this example, we'll change all these settings to French/Euro for a specific form.
+ */
+
+// TODO
+/*
+function simpay_custom_locale() {
+
+}
+
+add_filter( 'XXX', 'simpay_custom_locale' );
+
+function simpay_custom_date_format() {
+
+}
+
+add_filter( 'XXX', 'simpay_custom_date_format' );
+
+function simpay_custom_currency() {
+
+}
+
+add_filter( 'XXX', 'simpay_custom_currency' );
+*/
