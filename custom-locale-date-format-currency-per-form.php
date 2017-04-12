1efd5c9d81186b4c95293a86737e458811423ee9
Nick Young <mycorpweb@gmail.com>
2017-04-12 17:49:30 -0600

    Update French/Euro plugin

--- custom-locale-date-format-currency-per-form.php
+++ custom-locale-date-format-currency-per-form.php
@@ -8,25 +8,75 @@
 
 /**
  * In this example, we'll change all these settings to French/Euro for a specific form.
+ *
+ * Replace XXX in filter names to your form ID.
  */
 
-// TODO
-/*
+/**
+ * Apply form locale
+ *
+ * @return string
+ */
 function simpay_custom_locale() {
-
+	return 'fr';
 }
+add_filter( 'simpay_form_XXX_locale', 'simpay_custom_locale' );
 
-add_filter( 'XXX', 'simpay_custom_locale' );
-
+/**
+ * Apply form date filter
+ *
+ * Date formatting options: TODO link to jQuery page
+ *
+ * @return string
+ */
 function simpay_custom_date_format() {
-
+	return 'yy/mm/dd';
 }
+add_filter( 'simpay_form_XXX_date_format', 'simpay_custom_date_format' );
 
-add_filter( 'XXX', 'simpay_custom_date_format' );
-
+/**
+ * Apply the form currency filter
+ *
+ * @return string
+ */
 function simpay_custom_currency() {
-
+	return 'EUR';
 }
+add_filter( 'simpay_form_XXX_currency', 'simpay_custom_currency' );
 
-add_filter( 'XXX', 'simpay_custom_currency' );
-*/
+/**
+ * Apply the form currency position filter to set the currency symbol to the right side.
+ *
+ * Acceptable values:
+ * - left
+ * - left_space
+ * - right
+ * - right_space
+ *
+ * @return string
+ */
+function simpay_custom_currency_position() {
+	return 'right';
+}
+add_filter( 'simpay_form_XXX_currency_position', 'simpay_custom_currency_position' );
+
+/**
+ * Apply the form thousand separator filter to switch our thousands to a decimal point.
+ *
+ * @return string
+ */
+function simpay_custom_thousand_separator() {
+	return '.';
+}
+add_filter( 'simpay_form_XXX_thousand_separator', 'simpay_custom_thousand_separator' );
+
+
+/**
+ * Apply the form decimal separator filter to switch our decimal to a comma.
+ *
+ * @return string
+ */
+function simpay_custom_decimal_separator() {
+	return ',';
+}
+add_filter( 'simpay_form_XXX_decimal_separator', 'simpay_custom_decimal_separator' );
