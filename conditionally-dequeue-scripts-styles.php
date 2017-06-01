b5fd05dd54b494fe5bbd1993a7085b4aa570cb02
Nick Young <mycorpweb@gmail.com>
2017-06-01 15:47:56 -0600

    Add filter examples for removing scripts and styles

--- conditionally-dequeue-scripts-styles.php
+++ conditionally-dequeue-scripts-styles.php
@@ -27,3 +27,32 @@ function simpay_custom_dequeue_scripts_styles() {
 add_action( 'wp_print_scripts', 'simpay_custom_dequeue_scripts_styles', 100 );
 add_action( 'wp_print_styles', 'simpay_custom_dequeue_scripts_styles', 100 );
 */
+
+// The $scripts parameter is an array of all the scripts that need to be loaded for Simple Pay
+function simpay_custom_remove_scripts( $scripts ) {
+
+	if ( ! is_page( 'payment-page' ) ) {
+
+		// If we don't want to load any scripts then we can just return an empty array.
+		return array();
+	}
+
+	// By default we return the $scripts array that is setup through the plugin
+	return $scripts;
+}
+add_filter( 'simpay_before_register_public_scripts', 'simpay_custom_remove_scripts' );
+
+
+// The $style parameter is an array of all the styles that need to be loaded for Simple Pay
+function simpay_custom_remove_styles( $styles ) {
+
+	if ( ! is_page( 'payment-page' ) ) {
+
+		// If we don't want to load any styles then we can just return an empty array.
+		return array();
+	}
+
+	// By default we return the $styles array that is setup through the plugin
+	return $styles;
+}
+add_filter( 'simpay_before_register_public_styles', 'simpay_custom_remove_styles' );
