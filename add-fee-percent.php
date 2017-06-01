e981b9b22aa23686db6ffbce849948d045294656
Nick Young <mycorpweb@gmail.com>
2017-06-01 12:31:35 -0600

    Add fee percent example file

---
+++ add-fee-percent.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: WP Simple Pay - Add Fee Percent
+ * Plugin URI: https://wpsimplepay.com
+ * Description: Add a fee percent to all of your charges for WP Simple Pay
+ * Version: 1.0
+ */
+
+/**
+ * In this example, we'll see how to add a fee percent to all forms on the site.
+ *
+ * Note: Does not work for subscription charges.
+ */
+function simpay_add_percent_fee() {
+	// Needs to return the fee percent
+	return 10; // This translates to 10 percent, so a form at $10.00 will now be $11.00
+}
+add_filter( 'simpay_fee_percent', 'simpay_add_percent_fee' );
+
