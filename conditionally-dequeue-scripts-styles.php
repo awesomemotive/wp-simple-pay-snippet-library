94c4960f22a0a9f6783fddaaf6aa30f6df4d2ee1
Nick Young <mycorpweb@gmail.com>
2017-06-03 01:07:53 -0600

    Update dequeue scripts file

--- conditionally-dequeue-scripts-styles.php
+++ conditionally-dequeue-scripts-styles.php
@@ -7,26 +7,9 @@
  */
 
 /**
- * In this example, we'll change dequeue all plugin scripts & styles unless on a specific page with the slug
+ * In this example, we'll remove all plugin scripts & styles unless on a specific page with the slug
  * "payment-page". Available WP conditional tags: https://codex.wordpress.org/Conditional_Tags
  */
-
-// TODO
-/*
-function simpay_custom_dequeue_scripts_styles() {
-
-	if ( ! is_page( 'payment-page' ) ) {
-
-		// TODO Can we just run one call each to wp_dequeue_style & wp_dequeue_script?
-		// TODO Do we need to also call wp_deregister_style & wp_deregister_script?
-		wp_dequeue_style( 'XXX' );
-		wp_dequeue_script( 'XXX' );
-	}
-}
-
-add_action( 'wp_print_scripts', 'simpay_custom_dequeue_scripts_styles', 100 );
-add_action( 'wp_print_styles', 'simpay_custom_dequeue_scripts_styles', 100 );
-*/
 
 // The $scripts parameter is an array of all the scripts that need to be loaded for Simple Pay
 function simpay_custom_remove_scripts( $scripts ) {
