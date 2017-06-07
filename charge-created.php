e8fcc7a0570014ae964a66c5b9055de982db397e
Nick Young <mycorpweb@gmail.com>
2017-06-07 15:31:54 -0600

    Update charge created example

--- charge-created.php
+++ charge-created.php
@@ -9,15 +9,20 @@
 /**
  * In this example, we'll see how to send an email to the site admin after a charge has been successfully created.
  */
-function simpay_custom_send_email_on_charge() {
+function simpay_custom_send_email_on_charge( $charge ) {
+
+	$url = site_url();
+	$amount = simpay_formatted_amount( $charge->amount );
+	$email = $charge->receipt_email;
 
 	$to = get_bloginfo( 'admin_email' );
 	$subject = 'New WP Simple Pay Charge';
-	$body = 'A charge has been made on your site.';
+	$body = sprintf( 'You have just received a payment of %1$s by %2$s on %3$s', $amount, $email, $url );
 	$headers = array('Content-Type: text/html; charset=UTF-8');
 
 	wp_mail( $to, $subject, $body, $headers );
 
 }
-add_filter( 'simpay_charge_created', 'simpay_custom_send_email_on_charge' );
+add_action( 'simpay_charge_created', 'simpay_custom_send_email_on_charge' );
+
 
