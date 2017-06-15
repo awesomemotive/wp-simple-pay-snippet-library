1d8e5d37a85631583bbd7a3b8eb57790c3c78233
Nick Young <mycorpweb@gmail.com>
2017-06-14 20:36:18 -0600

    Add example for changing form specific success page URLs

---
+++ custom-success-page-url.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: WP Simple Pay - Success Page URLs
+ * Plugin URI: https://wpsimplepay.com
+ * Description: Change the success page URLs for specific forms for WP Simple Pay
+ * Version: 1.0
+ */
+
+/**
+ * In this example, we'll see how to change the payment success page for a specific form ID
+ */
+
+/**
+ * Repeat this function setup for each specific form ID you need to change the URL for.
+ * If you need to forms to go to the same page you can use the same callback function instead of duplicating it.
+ * If you still want to show the payment details then make sure the redirect page URL has the shortcode [simpay_payment_receipt]
+ */
+
+function simpay_custom_form_157_payment_success_page() {
+
+	// Change the redirect URL
+	return 'http://www.mywebsite.com/somepage';
+}
+add_filter( 'simpay_form_157_payment_success_page', 'simpay_custom_form_157_payment_success_page' );
+
