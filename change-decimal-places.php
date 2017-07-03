66309b79ee878af25f2a8bebff8bc66e9cfd3295
Nick Young <mycorpweb@gmail.com>
2017-07-03 10:46:48 -0600

    Update decimal places example to a more practical example (0 decimals)

--- change-decimal-places.php
+++ change-decimal-places.php
@@ -13,7 +13,6 @@ function simpay_custom_set_decimal_places() {
 
 	// Return the number of decimal places we want to show
 	// NOTE: Won't update the Stripe Checkout Overlay button
-	// NOTE: Stripe rounds to the nearest 2 decimal places anyway.
-	return 4;
+	return 0;
 }
 add_filter( 'simpay_decimal_places', 'simpay_custom_set_decimal_places' );
