620666057e7a0b720ab0bdd1cea9f99731e753b4
Nick Young <mycorpweb@gmail.com>
2017-08-18 11:32:53 -0600

    Update to point out one-time payments

--- add-flat-fee-amount.php
+++ add-flat-fee-amount.php
@@ -2,12 +2,12 @@
 /**
  * Plugin Name: WP Simple Pay - Add Flat Fee Amount
  * Plugin URI: https://wpsimplepay.com
- * Description: Add a flat fee amount to all of your charges for WP Simple Pay
+ * Description: Add a flat fee amount to all of your one-time payment charges for WP Simple Pay
  * Version: 1.0
  */
 
 /**
- * In this example, we'll see how to add a flat fee amount to all forms on the site.
+ * In this example, we'll see how to add a flat fee amount to all one-time payment forms on the site.
  *
  * Note: Does not work for subscription charges.
  */
