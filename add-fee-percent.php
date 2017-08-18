f777dd96ce1c918051535f59c66e3fb2d3110d54
Nick Young <mycorpweb@gmail.com>
2017-08-18 12:59:06 -0600

    Remove global example for fee percent

--- add-fee-percent.php
+++ add-fee-percent.php
@@ -7,17 +7,6 @@
  */
 
 /**
- * In this example, we'll see how to add a fee percent to all one-time payment forms on the site.
- *
- * Note: Does not work for subscription charges.
- */
-function simpay_custom_add_fee_percent() {
-	// Needs to return the fee percent
-	return 10; // This translates to 10 percent, so a form at $10.00 will now be $11.00
-}
-add_filter( 'simpay_fee_percent', 'simpay_custom_add_fee_percent' );
-
-/**
  * In this example, we'll see how to add a fee percent to a specific one-time payment form (id: 157) on the site.
  *
  * Note: Does not work for subscription charges.
