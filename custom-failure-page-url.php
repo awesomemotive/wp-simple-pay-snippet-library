f8213cf964f336d9b2ff7ceb4f71d4c421f50c9f
Nick Young <mycorpweb@gmail.com>
2017-08-18 12:08:04 -0600

    Add external link example to failure page url filter

--- custom-failure-page-url.php
+++ custom-failure-page-url.php
@@ -7,6 +7,18 @@
  */
 
 /**
+ * In this example, we'll see how to change the site wide payment failure page to an external URL
+ */
+
+function simpay_custom_payment_failure_page() {
+
+	// Change the redirect URL
+	return 'http://www.otherwebsite.com/landing-page';
+}
+add_filter( 'simpay_payment_failure_page', 'simpay_custom_payment_failure_page' );
+
+
+/**
  * In this example, we'll see how to change the payment failure page for a specific form ID
  */
 
@@ -14,7 +26,6 @@
  * Repeat this function setup for each specific form ID you need to change the URL for.
  * If you need to forms to go to the same page you can use the same callback function instead of duplicating it.
  */
-
 function simpay_custom_form_157_payment_failure_page() {
 
 	// Change the redirect URL
