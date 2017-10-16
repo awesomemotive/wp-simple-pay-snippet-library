762d5ddd292ab6b0a91a68c0ac1b84b276aeead5
Nick Young <mycorpweb@gmail.com>
2017-10-16 10:16:20 -0600

    Update API Keys sample

--- change-api-keys.php
+++ change-api-keys.php
@@ -13,7 +13,7 @@
  */
 
 // Change the secret keys based on the page found and what mode we are in
-function simpay_custom_alternate_secret_key( $key ) {
+function simpay_custom_form_157_alternate_secret_key( $key ) {
 
 	// Check if we are in test mode and load the correct key
 	if ( simpay_is_test_mode() ) {
@@ -26,32 +26,26 @@ function simpay_custom_alternate_secret_key( $key ) {
 		$key = 'sk_live_987654321';
 	}
 
-	// If we are not on the specific page we just return the default key found in the admin settings
+	// return the key
 	return $key;
 }
-add_filter( 'simpay_secret_key', 'simpay_custom_alternate_secret_key' );
-// Specific form filter using the ID: add_filter( 'simpay_form_157_secret_key', 'simpay_custom_alternate_secret_key' );
+add_filter( 'simpay_form_157_secret_key', 'simpay_custom_form_157_alternate_secret_key' );
 
 // Change the live keys based on the page found and what mode we are in
-function simpay_custom_alternate_publishable_key( $key ) {
+function simpay_custom_form_157_alternate_publishable_key( $key ) {
 
-	// Check for the page that has the form we want to change the API keys on
-	if ( is_page( 'my-page-name' ) ) {
+	// $test_mode will be true or false depending on what mode is selected in the main plugin settings
+	if ( simpay_is_test_mode() ) {
 
-		// $test_mode will be true or false depending on what mode is selected in the main plugin settings
-		if ( simpay_is_test_mode() ) {
+		// We are in test mode so we set the new test mode publishable key
+		$key = 'pk_test_123456789';
+	} else {
 
-			// We are in test mode so we set the new test mode publishable key
-			$key = 'pk_test_123456789';
-		} else {
-
-			// We are in live mode so we set the new live mode publishable key
-			$key = 'pk_live_987654321';
-		}
+		// We are in live mode so we set the new live mode publishable key
+		$key = 'pk_live_987654321';
 	}
 
-	// If we are not on the specific page we just return the default key found in the admin settings
+	// return the key
 	return $key;
 }
-add_filter( 'simpay_publishable_key', 'simpay_custom_alternate_publishable_key' );
-// Specific form filter using the ID: add_filter( 'simpay_form_157_publishable_key', 'simpay_custom_alternate_publishable_key' );
+add_filter( 'simpay_form_157_publishable_key', 'simpay_custom_form_157_alternate_publishable_key' );
