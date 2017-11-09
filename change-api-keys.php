5e5d7ec352d9addf4d02d5ad2b85dc1850db315e
Phil Derksen <pderksen@gmail.com>
2017-11-09 11:27:10 -0800

    Alternate Stripe API keys: Switch function order, simplify function names, update comments.

--- change-api-keys.php
+++ change-api-keys.php
@@ -7,31 +7,11 @@
  */
 
 /**
- * In this example, we'll see how to change the Stripe API keys for a form on a specific page
- *
+ * In this example, we'll see how to change the Stripe API keys for a form on a specific page.
  */
 
-// Change the secret keys based on the page found and what mode we are in
-function simpay_custom_form_157_alternate_secret_key( $key ) {
-
-	// Check if we are in test mode and load the correct key
-	if ( simpay_is_test_mode() ) {
-
-			// We are in test mode so we set the new test mode secret key
-			$key = 'sk_test_123456789';
-		} else {
-
-		// We are in live mode so we set the new live mode secret key
-		$key = 'sk_live_987654321';
-	}
-
-	// return the key
-	return $key;
-}
-add_filter( 'simpay_form_157_secret_key', 'simpay_custom_form_157_alternate_secret_key' );
-
-// Change the live keys based on the page found and what mode we are in
-function simpay_custom_form_157_alternate_publishable_key( $key ) {
+// Change the publishable keys based on the page found and what mode we are in.
+function simpay_custom_publishable_key( $key ) {
 
 	// $test_mode will be true or false depending on what mode is selected in the main plugin settings
 	if ( simpay_is_test_mode() ) {
@@ -47,4 +27,27 @@ function simpay_custom_form_157_alternate_publishable_key( $key ) {
 	// return the key
 	return $key;
 }
-add_filter( 'simpay_form_157_publishable_key', 'simpay_custom_form_157_alternate_publishable_key' );
+
+// Replace 157 with the form ID to apply alternate API keys to.
+add_filter( 'simpay_form_157_publishable_key', 'simpay_custom_publishable_key' );
+
+// Change the secret keys based on the page found and what mode we are in.
+function simpay_custom_secret_key( $key ) {
+
+	// Check if we are in test mode and load the correct key
+	if ( simpay_is_test_mode() ) {
+
+		// We are in test mode so we set the new test mode secret key
+		$key = 'sk_test_123456789';
+	} else {
+
+		// We are in live mode so we set the new live mode secret key
+		$key = 'sk_live_987654321';
+	}
+
+	// return the key
+	return $key;
+}
+
+// Replace 157 with the form ID to apply alternate API keys to.
+add_filter( 'simpay_form_157_secret_key', 'simpay_custom_secret_key' );
