a2c35bf62883dedc19a12754a2cf54ba19125933
Phil Derksen <pderksen@gmail.com>
2018-06-26 11:04:54 -0700

    Corrections & clarifications to percent fee & amount fee filters.

--- add-fee-percent.php
+++ add-fee-percent.php
@@ -1,19 +1,21 @@
 <?php
 /**
- * Plugin Name: WP Simple Pay - Add Fee Percent
+ * Plugin Name: WP Simple Pay - Add Percent Fee (non-global)
  * Plugin URI: https://wpsimplepay.com
- * Description: Add a fee percent to one-time payment charges for WP Simple Pay
+ * Description: Add a percent fee to specific one-time payment forms for WP Simple Pay.
  * Version: 1.0
  */
 
 /**
- * In this example, we'll see how to add a fee percent to a specific one-time payment form on the site.
+ * Add a percent fee to a specific one-time payment form that's separate from the global
+ * tax rate percentage value.
  *
  * Replace 157 with the form ID to target.
  * Note: Does not work for subscription charges.
  */
 function simpay_custom_form_157_fee_percent() {
-	// Needs to return the additional amount IN CENTS
-	return 300; // This will be an additional 3.00
+
+	return 7.5; // This will be an additional 7.5 %
 }
+
 add_filter( 'simpay_form_157_fee_percent', 'simpay_custom_form_157_fee_percent' );
--- add-flat-fee-amount.php
+++ add-flat-fee-amount.php
@@ -2,30 +2,35 @@
 /**
  * Plugin Name: WP Simple Pay - Add Flat Fee Amount
  * Plugin URI: https://wpsimplepay.com
- * Description: Add a flat fee amount to one-time payment charges for WP Simple Pay
+ * Description: Add a flat fee amount to one-time payment forms for WP Simple Pay.
  * Version: 1.0
  */
 
 /**
- * In this example, we'll see how to add a flat fee amount to all one-time payment forms on the site.
+ * Add a flat fee amount to all one-time payment forms on the site.
+ * TODO: Can be combined with a tax rate percentage value set in the global settings.
  *
  * Note: Does not work for subscription charges.
  */
 function simpay_custom_add_fee_amount() {
+
 	// Needs to return the additional amount IN CENTS
 	return 300; // This will be an additional 3.00
 }
+
 add_filter( 'simpay_fee_amount', 'simpay_custom_add_fee_amount' );
 
-
 /**
- * In this example, we'll see how to add a flat fee amount to a specific one-time payment form on the site.
+ * Add a flat fee amount to a specific one-time payment form.
+ * TODO: Can be combined with a percentage value set for the same payment form via a filter.
  *
  * Replace 157 with the form ID to target.
  * Note: Does not work for subscription charges.
  */
 function simpay_custom_form_157_fee_amount() {
+
 	// Needs to return the additional amount IN CENTS
 	return 300; // This will be an additional 3.00
 }
+
 add_filter( 'simpay_form_157_fee_amount', 'simpay_custom_form_157_fee_amount' );
