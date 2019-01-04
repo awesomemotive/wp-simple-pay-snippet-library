c42d7ba35bea7df3c68590aa2ca0bc0fc9269340
Phil Derksen <pderksen@gmail.com>
2019-01-04 14:15:27 -0800

    Comments

--- change-custom-amount-field-type.php
+++ change-custom-amount-field-type.php
@@ -9,6 +9,7 @@
 /**
  * In this example, we'll see how to change the custom amount field to type="number".
  * Default "tel" input type brings up number pad but does not allow decimal entry on mobile browsers.
+ * If type="number", automatically add step="0.01" attribute.
  */
 function simpay_set_custom_amount_field_type() {
 
