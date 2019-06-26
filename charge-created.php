2b71d6500aa641d2278ea88440a6cf67db459572
Spencer Finnell <spencerfinnell@gmail.com>
2019-06-26 13:05:00 -0400

    Update charge-created.php

--- charge-created.php
+++ charge-created.php
@@ -25,10 +25,11 @@ add_action( 'simpay_charge_created', 'simpay_do_something_after_charge' );
  * Send an email to the site admin after a charge has been successfully created.
  */
 function simpay_custom_send_email_on_charge( $charge ) {
-
 	$url    = site_url();
-	$amount = simpay_formatted_amount( $charge->amount );
-	$email  = $charge->receipt_email;
+	$amount = simpay_format_currency( $charge->amount );
+
+	$customer = \SimplePay\Core\Payments\Stripe_API::request( 'Customer', 'retrieve', $charge->customer );
+	$email    = $customer->email;
 
 	$to      = get_bloginfo( 'admin_email' );
 	$subject = 'New WP Simple Pay Charge';
@@ -36,7 +37,5 @@ function simpay_custom_send_email_on_charge( $charge ) {
 	$headers = array( 'Content-Type: text/html; charset=UTF-8' );
 
 	wp_mail( $to, $subject, $body, $headers );
-
 }
-
 add_action( 'simpay_charge_created', 'simpay_custom_send_email_on_charge' );
