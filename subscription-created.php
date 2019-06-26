88d64dd7511c8b90a5576f986b96448ee56b0a51
Spencer Finnell <spencerfinnell@gmail.com>
2019-06-26 13:03:48 -0400

    Update subscription-created.php

--- subscription-created.php
+++ subscription-created.php
@@ -7,21 +7,21 @@
  */
 
 /**
- * In this example, we'll see how to send an email to the site admin after a subscription charge has been successfully created.
+ * Send an email to the site admin after a charge has been successfully created.
  */
-function simpay_custom_send_email_on_subscription( $subscription, $customer ) {
+function simpay_custom_send_email_on_charge( $charge ) {
+	$url    = site_url();
+	$amount = simpay_format_currency( $charge->amount );
 
-	$url = site_url();
-	$plan_name = $subscription->plan->name;
-	$email = $customer->email;
+	// Retrieve the full customer object.
+	$customer = \SimplePay\Core\Payments\Stripe_API::request( 'Customer', 'retrieve', $charge->customer );
+	$email    = $customer->email;
 
-	$to = get_bloginfo( 'admin_email' );
-	$subject = 'New WP Simple Pay Subscription';
-	$body = sprintf( 'You have just received a subscription signup to %1$s by %2$s on %3$s', $plan_name, $email, $url );
-	$headers = array('Content-Type: text/html; charset=UTF-8');
+	$to      = get_bloginfo( 'admin_email' );
+	$subject = 'New WP Simple Pay Charge';
+	$body    = sprintf( 'You have just received a payment of %1$s by %2$s on %3$s', $amount, $email, $url );
+	$headers = array( 'Content-Type: text/html; charset=UTF-8' );
 
 	wp_mail( $to, $subject, $body, $headers );
-
 }
-add_action( 'simpay_subscription_created', 'simpay_custom_send_email_on_subscription', 10, 2 );
-
+add_action( 'simpay_charge_created', 'simpay_custom_send_email_on_charge' );
