11fd4c2ecb4bae954bf63df5701c1fc553559680
Spencer Finnell <spencerfinnell@gmail.com>
2019-09-16 11:24:14 -0400

    Create custom-decimal-separator.php

---
+++ plugins/custom-decimal-separator.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: WP Simple Pay - Comma Decimal Separator
+ * Plugin URI: https://wpsimplepay.com
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Description: Change the decimal separator using a filter for WP Simple Pay
+ * Version: 1.0
+ */
+
+/**
+ * Changes the decimal separatator from a . (period) to a , (comma).
+ *
+ * @param string $separator Decimal separator.
+ * @return string
+ */
+function simpay_custom_decimal_separator( $separator ) {
+	return ',';
+}
+add_filter( 'simpay_decimal_separator', 'simpay_custom_decimal_separator' );
