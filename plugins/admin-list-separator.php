38818b7525b23063354c25600d48efb2208e8f7c
Spencer Finnell <spencerfinnell@gmail.com>
2019-09-16 10:53:03 -0400

    Update admin-list-separator.php

--- plugins/admin-list-separator.php
+++ plugins/admin-list-separator.php
@@ -2,18 +2,24 @@
 /**
  * Plugin Name: WP Simple Pay - Admin List Separators
  * Plugin URI: https://wpsimplepay.com
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
  * Description: Use a different delimiter for admin fields that use list-type values.
  * Version: 1.0
  */
 
 /**
- * In this example, we'll see how to change the default comma for separating list-type values
+ * Changes the way single-line values and determined in custom fields.
  *
- *  Affected Custom Fields: Dropdown values/amounts/quantities, radio values/amounts/quantities
+ * 1,2,3 in to 1|2|3
+ *
+ * Affected Custom Fields:
+ *   - Dropdown values, amounts, quantities.
+ *   - Radio values, amounts, quantities.
+ *
+ * @param string $separator List separator.
  */
-
-// We will change it from the default comma (,) to use a pipe (|)
-function simpay_custom_list_separator() {
+function simpay_custom_list_separator( $separator ) {
 	return '|';
 }
 add_filter( 'simpay_list_separator', 'simpay_custom_list_separator' );
