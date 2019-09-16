40d2ff57953ef4fd7ea5c24ad4f720ab350919c8
Spencer Finnell <spencerfinnell@gmail.com>
2019-09-16 10:40:04 -0400

    Update add-flat-fee-amount.php

--- plugins/add-flat-fee-amount.php
+++ plugins/add-flat-fee-amount.php
@@ -2,33 +2,23 @@
 /**
  * Plugin Name: WP Simple Pay - Add Flat Fee Amount
  * Plugin URI: https://wpsimplepay.com
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
  * Description: Add a flat fee amount to one-time payment forms.
  * Version: 1.0
  */
 
 /**
- * Add a flat fee amount to all one-time payment forms on the site.
- *
- * Note: Does not work for subscription charges AND are calculated pre-tax.
- */
-function simpay_custom_add_fee_amount() {
-
-	// Needs to return the additional amount IN CENTS
-	return 300; // This will be an additional 3.00
-}
-
-add_filter( 'simpay_fee_amount', 'simpay_custom_add_fee_amount' );
-
-/**
- * Add a flat fee amount to a specific one-time payment form.
+ * Adds a flat fee to a specific one-time payment form in addition taxes.
  *
  * Replace 157 with the form ID to target.
- * Note: Does not work for subscription charges AND are calculated pre-tax.
+ *
+ * Note: Does not work for subscription charges.
+ *
+ * @return float
  */
 function simpay_custom_form_157_fee_amount() {
-
-	// Needs to return the additional amount IN CENTS
-	return 300; // This will be an additional 3.00
+	// This will be an additional $3.00 to the total price.
+	return 3;
 }
-
 add_filter( 'simpay_form_157_fee_amount', 'simpay_custom_form_157_fee_amount' );
