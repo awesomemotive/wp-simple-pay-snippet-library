dd81f29ef7c9b7bb281968944c09dcb6bb70231e
Spencer Finnell <spencerfinnell@gmail.com>
2019-09-16 11:17:47 -0400

    Create custom-custom-amount-field-type.php

---
+++ plugins/custom-custom-amount-field-type.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: WP Simple Pay - &lt;input type="number" /&gt; Custom Amount Field
+ * Plugin URI: https://wpsimplepay.com
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Description: Change the type of the custom amount field to a number field.
+ * Version: 1.0
+ */
+
+/**
+ * Changes the `type` attribute on the `input` tag.
+ *
+ * @param string $input_type Input type.
+ * @return string
+ */
+function simpay_set_custom_amount_field_type( $input_Type ) {
+	return 'number';
+}
+add_filter( 'simpay_custom_amount_field_type', 'simpay_set_custom_amount_field_type' );
