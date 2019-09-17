6d6b74a59851f0418be3b44216e3f02b19a7e943
Spencer Finnell <spencerfinnell@gmail.com>
2019-09-17 12:10:59 -0400

    Update custom-failure-page-url.php

--- plugins/custom-failure-page-url.php
+++ plugins/custom-failure-page-url.php
@@ -2,34 +2,21 @@
 /**
  * Plugin Name: WP Simple Pay - Failure Page URLs
  * Plugin URI: https://wpsimplepay.com
- * Description: Change the failure page URLs for WP Simple Pay
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Description: Change the failure page URLs for WP Simple Pay.
  * Version: 1.0
  */
 
 /**
- * In this example, we'll see how to change the site wide payment failure page to an external URL
+ * Changes the failure URL for form 157.
+ *
+ * Replace 157 with the ID of your form.
+ *
+ * @param string $url Failure URL.
+ * @return string
  */
-
-function simpay_custom_payment_failure_page() {
-
-	// Change the redirect URL
-	return 'http://www.otherwebsite.com/landing-page';
-}
-add_filter( 'simpay_payment_failure_page', 'simpay_custom_payment_failure_page' );
-
-
-/**
- * In this example, we'll see how to change the payment failure page for a specific form ID
- */
-
-/**
- * Repeat this function setup for each specific form ID you need to change the URL for.
- * If you need to forms to go to the same page you can use the same callback function instead of duplicating it.
- */
-function simpay_custom_form_157_payment_failure_page() {
-
-	// Change the redirect URL
+function simpay_custom_form_157_payment_failure_page( $url ) {
 	return 'http://www.mywebsite.com/somepage';
 }
 add_filter( 'simpay_form_157_payment_failure_page', 'simpay_custom_form_157_payment_failure_page' );
-
