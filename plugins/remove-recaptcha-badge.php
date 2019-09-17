377126535aedda6582bc9655d7b690be142def8f
Spencer Finnell <spencerfinnell@gmail.com>
2019-09-17 15:11:52 -0400

    Update remove-recaptcha-badge.php

--- plugins/remove-recaptcha-badge.php
+++ plugins/remove-recaptcha-badge.php
@@ -2,15 +2,18 @@
 /**
  * Plugin Name: WP Simple Pay - Hide reCAPTCHA Badge
  * Plugin URI: https://wpsimplepay.com
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
  * Description: Hide reCAPTCHA badge and add links to privacy policy and terms of service.
  * Version: 1.0
  */
 
 /**
- * Add CSS to hide the badge and add links to Google terms and policies.
+ * Adds CSS to hide the badge and add links to Google terms and policies.
  */
 function simpay_remove_recaptcha_badge() {
 ?>
+
 <style>.grecaptcha-badge { display: none; }</style>
 
 <p>This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy">Privacy Policy</a> and <a href="https://policies.google.com/terms">Terms of Service</a> apply.</p>
