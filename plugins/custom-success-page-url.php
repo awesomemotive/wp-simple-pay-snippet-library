bb74de867d7046c754767d69620411220fca8fd8
Spencer Finnell <spencerfinnell@gmail.com>
2019-09-17 14:31:17 -0400

    Update custom-success-page-url.php

--- plugins/custom-success-page-url.php
+++ plugins/custom-success-page-url.php
@@ -1,36 +1,22 @@
 <?php
 /**
- * Plugin Name: WP Simple Pay - Success Page URLs
+ * Plugin Name: WP Simple Pay - Success Page URL
  * Plugin URI: https://wpsimplepay.com
- * Description: Change the success page URLs for WP Simple Pay
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Description: Change the success page URLs for WP Simple Pay.
  * Version: 1.0
  */
 
 /**
- * In this example, we'll see how to change the site wide payment success page to an external URL
+ * Changes the success URL for form 157.
+ *
+ * Replace 157 with the ID of your form.
+ *
+ * @param string $url Failure URL.
+ * @return string
  */
-
-function simpay_custom_payment_success_page() {
-
-	// Change the redirect URL
-	return 'http://www.otherwebsite.com/landing-page';
-}
-add_filter( 'simpay_payment_success_page', 'simpay_custom_payment_success_page' );
-
-
-/**
- * In this example, we'll see how to change the payment success page for a specific form ID
- */
-
-/**
- * Repeat this function setup for each specific form ID you need to change the URL for.
- * If you need to forms to go to the same page you can use the same callback function instead of duplicating it.
- * If you still want to show the payment details then make sure the redirect page URL has the shortcode [simpay_payment_receipt]
- */
-function simpay_custom_form_157_payment_success_page() {
-
-	// Change the redirect URL
+function simpay_custom_form_157_payment_success_page( $url ) {
 	return 'http://www.mywebsite.com/somepage';
 }
 add_filter( 'simpay_form_157_payment_success_page', 'simpay_custom_form_157_payment_success_page' );
-
