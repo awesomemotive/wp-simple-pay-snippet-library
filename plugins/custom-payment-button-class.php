68057b06fd35aa670f0d848fb76d57b74930e3b9
Spencer Finnell <spencerfinnell@gmail.com>
2019-09-17 13:49:20 -0400

    Update custom-payment-button-class.php

--- plugins/custom-payment-button-class.php
+++ plugins/custom-payment-button-class.php
@@ -2,19 +2,22 @@
 /**
  * Plugin Name: WP Simple Pay - Custom Form Button Class
  * Plugin URI: https://wpsimplepay.com
- * Description: Custom form button class for WP Simple Pay
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Description: Add custom class to the payment button.
  * Version: 1.0
  */
 
 /**
- * In this example, we'll see how to add a class to the on-page form payment button.
+ * Adds the class `my-button-class` to Payment Buttons.
+ *
+ * @param array $classes List of class names.
+ * @return array
  */
 function simpay_custom_add_payment_button_class( $classes ) {
-	// Add 1 class.
-	return array_merge( $classes, array( 'my-button-class-1' ) );
+	$classes['my-button-class-1'];
+	$classes['my-button-class-2'];
 
-	// Add multiple classes.
-	//return array_merge( $classes, array( 'my-button-class-1', 'my-button-class-2' ) );
+	return $classes;
 }
-
 add_filter( 'simpay_payment_button_class', 'simpay_custom_add_payment_button_class' );
