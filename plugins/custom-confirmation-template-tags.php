0eb5f139022252e6645bb6005374c44720e8a9fc
Spencer Finnell <spencerfinnell@gmail.com>
2019-09-24 12:55:45 -0400

    Show how to retrieve metadata in template tags

--- plugins/custom-confirmation-template-tags.php
+++ plugins/custom-confirmation-template-tags.php
@@ -21,6 +21,9 @@ function simpay_custom_template_tags( $tags ) {
 	// "Name" field that appears on Embed/Overlay forms.
 	$tags[] = 'full-name';
 
+	// Custom field being collected.
+	$tags[] = 'custom-meta-key';
+
 	return $tags;
 }
 add_filter( 'simpay_payment_details_template_tags', 'simpay_custom_template_tags' );
@@ -71,3 +74,31 @@ function simpay_custom_template_tag_full_name( $value, $payment_confirmation_dat
 	return $payment_confirmation_data['customer']->name;
 }
 add_filter( 'simpay_payment_confirmation_template_tag_full-name', 'simpay_custom_template_tag_full_name', 10, 2 );
+
+/**
+ * Replaces the {custom-meta-key} template tag with the value of the metadata.
+ *
+ * @param string $value Template tag value.
+ * @param array  $payment_confirmation_data {
+ *   Contextual information about this payment confirmation.
+ *
+ *   @type \Stripe\Customer               $customer Stripe Customer
+ *   @type \SimplePay\Core\Abstracts\Form $form Payment form.
+ *   @type object                         $subscriptions Subscriptions associated with the Customer.
+ *   @type object                         $paymentintents PaymentIntents associated with the Customer.
+ * }
+ * @return string
+ */
+function simpay_custom_template_tag_custom_meta_key( $value, $payment_confirmation_data ) {
+	$custom_meta_key = 'Custom Meta Key';
+
+	// Show Subscription metadata.
+	if ( ! empty( $payment_confirmation_data['subscriptions'] ) ) {
+		return current( $payment_confirmation_data['subscriptions'] )->metadata[ $custom_meta_key ];
+	
+	// Show single payment metadata.
+	} else {
+		return current( $payment_confirmation_data['paymentintents'] )->metadata[ $custom_meta_key ];
+	}
+}
+add_filter( 'simpay_payment_confirmation_template_tag_custom-meta-key', 'simpay_custom_template_tag_custom_meta_key', 10, 2 );
