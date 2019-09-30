977b03c7450774fa7b0d5c53507f1653fb224089
Spencer Finnell <spencerfinnell@gmail.com>
2019-09-30 13:37:38 -0400

    Auto apply a coupon to a specific form

---
+++ plugins/add-default-coupon.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: WP Simple Pay - Apply Default Coupon
+ * Plugin URI: https://wpsimplepay.com
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Description: Auto apply a default coupon.
+ * Version: 1.0
+ */
+
+/**
+ * Adds and applies a coupon to a form.
+ *
+ * Replace 157 with the form ID to target.
+ * Replace 44off with the ID of your coupon.
+ */
+function simpay_custom_add_default_coupon() {
+	$form_id = 157;
+	$coupon  = '44off';
+
+	wp_add_inline_script(
+		'simpay-public-pro',
+		"jQuery( document ).on( 'simpayBindCoreFormEventsAndTriggers', function( e, spFormElem, formData ) {
+			if ( {$form_id} !== formData.formId ) {
+				return;
+			}
+			spFormElem.find( '.simpay-coupon-field' ).val( '{$coupon}' );
+			spFormElem.find( '.simpay-apply-coupon' ).trigger( 'click' );
+		} );"	
+	);
+}
+add_action( 'wp_enqueue_scripts', 'simpay_custom_add_default_coupon', 20 );
