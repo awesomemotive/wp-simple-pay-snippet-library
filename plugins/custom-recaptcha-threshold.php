aa778fcee5b6d614d1f226e648229b8560a3917e
Spencer Finnell <spencerfinnell@gmail.com>
2020-03-30 13:28:47 -0400

    Create custom-recaptcha-threshold.php

---
+++ plugins/custom-recaptcha-threshold.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: WP Simple Pay - reCAPTCHA Threshold
+ * Plugin URI: https://wpsimplepay.com
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Description: Changes the reCAPTCHA v3 threshold.
+ * Version: 1.0
+ */
+
+/**
+ * Adjusts the reCAPTCHA v3 threshold for determing fraudulant interactions.
+ *
+ * @link https://developers.google.com/recaptcha/docs/v3
+ *
+ * @param string $treshold Threshold. 0-1. Default 0.5
+ * @return string
+ */
+function custom_threshold( $threshold ) {
+	return '0.7';
+}
+add_filter( 'simpay_recpatcha_minimum_score', __NAMESPACE__ . '\\custom_threshold' );
