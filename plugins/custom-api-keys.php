7a256038c0914b03e4dc8552b5893cfed556d134
Spencer Finnell <spencerfinnell@gmail.com>
2020-07-06 14:38:25 -0400

    Update custom-api-keys.php

--- plugins/custom-api-keys.php
+++ plugins/custom-api-keys.php
@@ -5,7 +5,7 @@
  * Author: Sandhills Development, LLC
  * Author URI: https://sandhillsdev.com
  * Description: Use unique Stripe API keys for a specific form.
- * Version: 1.0
+ * Version: 2.0
  */
 
 /**
@@ -16,20 +16,18 @@
  * @param string $key Stripe API key.
  * @return string
  */
-function simpay_form_157_publishable_key( $key ) {
-
-	// We are in test mode so we set the new test mode publishable key.
+add_filter( 'simpay_form_157_publishable_key', function( $key ) {
+	// We are in _global_ test mode so we set the new test mode publishable key.
 	if ( simpay_is_test_mode() ) {
 		$key = 'pk_test_123456789';
 
-	// We are in live mode so we set the new live mode publishable key.
+	// We are in _global_ live mode so we set the new live mode publishable key.
 	} else {
 		$key = 'pk_live_987654321';
 	}
 
 	return $key;
-}
-add_filter( 'simpay_form_157_publishable_key', 'simpay_form_157_publishable_key' );
+} );
 
 /**
  * Changes the Stripe Secret key for form 157.
@@ -39,51 +37,15 @@ add_filter( 'simpay_form_157_publishable_key', 'simpay_form_157_publishable_key'
  * @param string $key Stripe API key.
  * @return string
  */
-function simpay_form_157_secret_key( $key ) {
-
-	// We are in test mode so we set the new test mode publishable key.
+add_filter( 'simpay_form_157_secret_key', function( $key ) {
+	// We are in _global_ test mode so we set the new test mode publishable key.
 	if ( simpay_is_test_mode() ) {
 		$key = 'sk_test_123456789';
 
-	// We are in live mode so we set the new live mode publishable key.
+	// We are in _global_ live mode so we set the new live mode publishable key.
 	} else {
 		$key = 'sk_live_987654321';
 	}
 
 	return $key;
-}
-add_filter( 'simpay_form_157_secret_key', 'simpay_form_157_secret_key' );
-
-/**
- * Stub in support for legacy use of $simpay_form in simpay_get_publishable_key().
- *
- * Replace 157 with the ID of your form.
- */
-add_action( 'init', function() {
-	if ( ! isset( $_GET['form_id'] ) ) {
-		return;
-	}
-
-	global $simpay_form;
-
-	$form_id = absint( $_GET['form_id'] );
-
-	$simpay_form     = new \stdClass();
-	$simpay_form->id = $form_id;
-
-	if ( 157 === $form_id ) {
-
-		// We are in test mode so we set the new test mode publishable key.
-		if ( simpay_is_test_mode() ) {
-			$simpay_form->publishable_key = 'pk_test_123456789';
-			$simpay_form->secret_key = 'sk_test_123456789';
-
-		// We are in live mode so we set the new live mode publishable key.
-		} else {
-			$simpay_form->publishable_key = 'pk_live_987654321';
-			$simpay_form->secret_key = 'sk_live_987654321';
-		}
-		
-	}
-
 } );
