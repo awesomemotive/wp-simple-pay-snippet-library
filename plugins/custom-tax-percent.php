5e7ad682148fef1eb6d3b0c0d67a4dabd2a8b951
Spencer Finnell <spencerfinnell@gmail.com>
2021-03-17 12:03:31 -0400

    Update custom-tax-percent.php

--- plugins/custom-tax-percent.php
+++ plugins/custom-tax-percent.php
@@ -1,22 +1,55 @@
 <?php
 /**
- * Plugin Name: WP Simple Pay - Custom Tax Percent
+ * Plugin Name: WP Simple Pay - Custom Tax Rate Percentage
  * Plugin URI: https://wpsimplepay.com
  * Author: Sandhills Development, LLC
  * Author URI: https://sandhillsdev.com
- * Description: Override the global tax percent fee for a payment form.
- * Version: 1.0
+ * Description: Override the returned tax rate for a given Payment Form.
+ * Version: 2.0
  */
 
 /**
- * Override the global tax percent fee for a specific payment form.
- *
- * Replace 157 with the form ID to target.
- *
- * @param float $percent Tax percent.
- * @return float
+ * Important: Tax rates with the percentage to use must first be created in Simple Pay Pro > Settings > General > Taxes.
  */
-function simpay_custom_form_157_tax_percent( $percent ) {
-	return 7.5;
+
+/**
+ * Find a tax rate based on a given percentage.
+ * 
+ * @param int                            $percentage Tax rate percentage.
+ * @param \SimplePay\Pro\Taxes\TaxRate[] $tax_rates Tax Rates
+ */
+function simpay_find_tax_rate( $percentage, $tax_rates ) {
+	return array_filter(
+		$tax_rates,
+		function ( $tax_rate ) use ( $percentage ) {
+			return $tax_rate->percentage == $percentage;
+		}
+	);
 }
-add_filter( 'simpay_form_157_tax_percent', 'simpay_custom_form_157_tax_percent' );
+
+// Filter a Payment Form's prices.
+add_filter(
+	'simpay_get_payment_form_tax_rates',
+	/**
+	 * Filters the tax rates for a given form.
+	 * 
+	 * @param \SimplePay\Pro\Taxes\TaxRate[] $tax_rates Tax Rates
+	 * @param \SimplePay\Core\Abstracts\Form $form Payment Form.
+	 */
+	function( $tax_rates, $form ) {
+		switch ( $form->id ) {
+			// Return a tax rate of 10% for Payment Form 13.
+			case 13:
+				return simpay_find_tax_rate( 10, $tax_rates );
+				break;
+			// Return a tax rate of 12% for Payment Form 57
+			case 57:
+				return simpay_find_tax_rate( 12, $tax_rates );
+				break;
+			default:
+				return $tax_rates;
+		}
+	},
+	10,
+	2
+);
