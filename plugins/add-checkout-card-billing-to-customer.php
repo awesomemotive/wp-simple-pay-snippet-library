70093ca7c71545de6867afff1d89f427744d222d
Spencer Finnell <spencerfinnell@gmail.com>
2021-03-17 12:05:24 -0400

    Update add-checkout-card-billing-to-customer.php

--- plugins/add-checkout-card-billing-to-customer.php
+++ plugins/add-checkout-card-billing-to-customer.php
@@ -5,32 +5,7 @@
  * Author: Sandhills Development, LLC
  * Author URI: https://sandhillsdev.com
  * Description: Maps the billing address collected in Stripe Checkout to the Customer object.
- * Version: 2.0
+ * Version: 3.0
  */
-add_action( 'simpay_webhook_checkout_session_completed', function( $event, $customer ) {
-	$payment_methods = \SimplePay\Core\Payments\Stripe_API::request(
-		'PaymentMethod',
-		'all',
-		array(
-			'type'     => 'card',
-			'customer' => $customer->id,
-		)
-	);
 
-	$payment_method = current( $payment_methods->data );
-
-	$address = $payment_method->billing_details->address->toArray();
-	$name    = $payment_method->billing_details->name;
-	$phone   = $payment_method->billing_details->phone;
-
-	return \SimplePay\Core\Payments\Stripe_API::request(
-		'Customer',
-		'update',
-		$customer->id,
-		array(
-			'name'    => $name,
-			'address' => $address,
-			'phone'   => $phone,
-		)
-	);
-}, 10, 2 );
+// This snippet is no longer required in WP Simple Pay 4.1
