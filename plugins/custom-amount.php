9691890617216784d8106c6c1d13cb3bc54728c4
Spencer Finnell <spencerfinnell@gmail.com>
2021-12-06 11:01:55 -0500

    Update custom-amount.php

--- plugins/custom-amount.php
+++ plugins/custom-amount.php
@@ -9,14 +9,39 @@
  */
 
 /**
- * Changes the amount the form will charge.
+ * Adjusts the WP Simple Pay payment form amount.
  *
- * Replace 157 with the form ID to target.
+ * Update $form_id, $unit_amount, and $currency variables only.
  *
- * @param string $amount Amount set from the form settings.
- * @return string
+ * @param \SimplePay\Core\PaymentForm\PriceOption[] $price_options Payment form price options.
+ * @param \SimplePay\Core\Abstracts\Form            $form Payment form.
+ * @return \SimplePay\Core\PaymentForm\PriceOption[]
  */
-function simpay_custom_form_157_amount( $amount ) {
-	return '123.25';
-}
-add_filter( 'simpay_form_157_amount', 'simpay_custom_form_157_amount' );
+add_filter(
+	'simpay_get_payment_form_price_options',
+	function( $price_options, $form ) {
+		$form_id     = 150;
+		$unit_amount = 2000; // $20.00 in cents.
+		$currency    = 'usd';
+
+		// STOP EDITING.
+		if ( $form_id !== $form->id ) {
+			return $price_options;
+		}
+
+		$custom_amount = new \SimplePay\Core\PaymentForm\PriceOption(
+			array(
+				'id'              => 'simpay_custom_amount',
+				'currency'        => $currency,
+				'unit_amount'     => $unit_amount,
+				'unit_amount_min' => $unit_amount,
+				'default'         => true,
+			),
+			$form
+		);
+
+		return array( $custom_amount );
+	},
+	10,
+	2
+);
