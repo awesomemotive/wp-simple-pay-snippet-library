19c313c35ec9ed16b8caae0303d924a2ad9ec596
Spencer Finnell <spencerfinnell@gmail.com>
2021-12-14 09:39:08 -0500

    Create add-description-as-payment-description.php

---
+++ plugins/add-description-as-payment-description.php
@@ -0,0 +1,18 @@
+/**
+ * Plugin Name: WP Simple Pay - Use Payment Form Description as Payment Description
+ * Plugin URI: https://wpsimplepay.com
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Description: Custom item description.
+ * Version: 1.0
+ */
+add_filter(
+	'simpay_get_paymentintent_args_from_payment_form_request',
+	function( $args, $form ) {
+		$args['description'] = $form->item_description;
+
+		return $args;
+	},
+	10,
+	2
+);
