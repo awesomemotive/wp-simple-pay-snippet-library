727c36799369e194309af3236aed998fdeba27c2
Spencer Finnell <spencerfinnell@gmail.com>
2022-02-09 14:00:38 -0500

    Update custom-statement-descriptor.php

--- plugins/custom-statement-descriptor.php
+++ plugins/custom-statement-descriptor.php
@@ -8,6 +8,8 @@
  * Version: 1.0
  */
 
+// Use the "Title" field.
+
 /**
  * Adjust the statement descriptor for form 157.
  *
@@ -18,7 +20,32 @@
  * @param string $statement_descriptor Statement descriptor.
  * @return string
  */
-function simpay_custom_form_157_statement_descriptor( $statement_descriptor ) {
-	return get_bloginfo( 'name' );
-}
-add_filter( 'simpay_form_157_statement_descriptor', 'simpay_custom_form_157_statement_descriptor' );
+add_filter(
+	'simpay_form_157_statement_descriptor',
+	function( $value, $form_id ) {
+		return get_post_meta( $form_id, '_company_name', true ); // "Title" setting.
+	},
+	10,
+	2
+);
+
+// ... or use the "Description" field.
+
+/**
+ * Adjust the statement descriptor for form 157.
+ *
+ * Replace 157 with the form ID to target.
+ *
+ * @link https://stripe.com/docs/statement-descriptors
+ *
+ * @param string $statement_descriptor Statement descriptor.
+ * @return string
+ */
+add_filter(
+	'simpay_form_157_statement_descriptor',
+	function( $value, $form_id ) {
+		return get_post_meta( $form_id, '_item_description', true ); // "Description" setting.
+	},
+	10,
+	2
+);
